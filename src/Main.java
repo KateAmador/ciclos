433d932119a200753b7c4e6f1be56fdd98b67da0
KateAmador <kateamador13@gmail.com>
2022-06-25 08:23:28 -0500

    actualizacion

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
+import javax.swing.*;
+
 public class Main {
     public static void main(String[] args) {
-
+        for (int i = 0; i < 3; i++){
+            JOptionPane.showMessageDialog(null, "Hola Mundo");
+        }
     }
 }
